15ee7e2d3eabe068eb719cbb166dbba1c43a348c
Técnico em Informática 2018.1 <39121742018.1@senacrjedu.local>
2019-10-09 11:47:13 -0300

    andamento

---
+++ src/app/services/game.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
+import { Game } from '../model/game';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GameService {
+
+  constructor(
+    protected fire: AngularFirestore
+  ) { }
+
+  save(game) {
+    return this.fire.collection("games")
+      .add({
+        nome: game.nome,
+        descricao: game.descricao,
+        categoria: game.categoria,
+        console: game.console,
+        quant: game.quant,
+        valor: game.valor,
+        ativo: game.ativo
+      });
+  }
+
+  gelAll() {
+    return this.fire.collection("game").snapshotChanges()
+      .pipe(
+        map(dados =>
+          dados.map(d => ({ key: d.payload.doc.id, ...d.payload.doc.data() }))
+        )
+      )
+  }
+
+  get(id) {
+    return this.fire.collection("game").doc<Game>(id).valueChanges();
+  }
+  update(game: Game, id: string) {
+    return this.fire.collection("players").doc<Game>(id)
+      .update(game);
+  }
+
+  remove(game: any) {
+    return this.fire.collection("game").doc(game.key).delete();
+  }
+}
